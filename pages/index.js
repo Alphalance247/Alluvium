4d711d5ee8bd5f019b506fed9f5c4bf06a7024bd
timilxr <timilxr@gmail.com>
2022-08-22 21:22:47 +0100

    included the slider on homepage

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,62 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/homepage.module.scss'
 import Navigation from '../components/navigation';
-import ProductPreview from '../components/productPreview';
+import { Carousel } from 'react-responsive-carousel'
+
+// Import css files
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 // import useSWR from 'swr';
 // import { baseURL } from '../config'
-import { productData, featuresData, ourServicesData, milestonesData, trustedCompaniesData } from '../data'
+import { productData, featuresData, ourServicesData, milestonesData, trustedCompaniesData } from '../data';
+
+const HeroBanner = () => {
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.us}>
+          <h1>We are atlassian</h1>
+          {/* <h1> atlassian</h1> */}
+          <h1> migration experts</h1>
+        </div>
+        <div className={styles.message}>
+          <p>We are Atlassian Products <span className={styles.migration}>Migration Lab.</span> </p>
+          <p>We produce Tools, Systems and Services that deliver complete data migration in <span className={styles.half}>half the time for half the cost.</span> </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const EventAd = () => {
+  return (
+    <>
+      <div className={styles.eventBg}>
+        {/* <Image src={'/assets/home/graduation_bg.png'} height={1440} width={512} layout="fill" priority loading='eager' /> */}
+        <Image src={'/assets/home/alluve-bg.jpg'} layout="fill" priority loading='eager' />
+        <div className={styles.blazer}>
+          <Image src={'/assets/home/trailblazer.png'} height={97} width={150} priority loading='eager' />
+          <h2>Graduation Ceremony</h2>
+          <Link href="http://university.alluvium.net/event">
+            <a className={styles.reg}>Register for this Event</a>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const slideArr = [<HeroBanner />, <EventAd />];
 
 
 export default function Home({ products, features, ourServices, milestones, trustedCompanies }) {
   const [lightTheme, setLightTheme] = useState('')
 
-
   useEffect(() => {
     const theme = localStorage.getItem("light-mode");
     setLightTheme(JSON.parse(theme))
     console.log(lightTheme)
-  }, [])
-
-
+  }, []);
 
   return (
     <div>
@@ -35,22 +74,19 @@ export default function Home({ products, features, ourServices, milestones, trus
         <section className={styles.nav}>
           <Navigation products={products} />
         </section>
-        <section className={styles.hero}>
-          <div className={styles.us}>
-            <h1>We are atlassian</h1>
-            {/* <h1> atlassian</h1> */}
-            <h1> migration experts</h1>
-          </div>
-          <div className={styles.message}>
-            <p>We are Atlassian Products <span className={styles.migration}>Migration Lab.</span> </p>
-            <p>We produce Tools, Systems and Services that deliver complete data migration in <span className={styles.half}>half the time for half the cost.</span> </p>
-          </div>
-        </section>
+        <Carousel autoPlay infiniteLoop interval={3000} showStatus={false} showThumbs={false} dynamicHeight={true}>
+          {slideArr.map((x, i) => {
+            return <div key={i}>
+              {x}
+            </div>
+          })}
+        </Carousel>
+        {/* <HeroBanner /> */}
         <section className={styles.featureSection}>
           <h3 className={styles.featuresTitle}>Our Values</h3>
           <div className={styles.features}>
             <div className={styles.featureSectionImg} data-aos="zoom-in">
-            <Image src="/assets/home/values.png" width={654.26/1.5} height={625.37/1.5} priority loading='eager' />
+              <Image src="/assets/home/values.png" width={654.26 / 1.5} height={625.37 / 1.5} priority loading='eager' />
             </div>
           </div>
         </section>
