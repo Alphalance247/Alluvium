b008cca577f0825e810b37cc325d7db4e1cb5abb
mrprotocoll <lekanvgbg@gmail.com>
2022-12-23 09:03:11 +0100

    Add christmas designs

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,6 @@ import { Carousel } from 'react-responsive-carousel'
 
 // Import css files
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-// import useSWR from 'swr';
-// import { baseURL } from '../config'
 import { productData, featuresData, ourServicesData, milestonesData, trustedCompaniesData } from '../data';
 
 const HeroBanner = () => {
