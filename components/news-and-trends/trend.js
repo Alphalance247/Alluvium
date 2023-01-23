c23288b119c30d229ebaf430cdfd2547662da76f
timilxr <38140090+timilxr@users.noreply.github.com>
2023-01-23 11:55:18 +0100

    news trends

--- components/news-and-trends/trend.js
+++ components/news-and-trends/trend.js
@@ -1,28 +1,28 @@
 import styles from "../../styles/trend.module.scss";
 import Image from "next/image";
-import NewsLink from "./link";
+import Link from "next/link";
 import { dateFormat } from "lib/ga";
+import moment from "moment";
 
 const Trend = (props) => {
 	const { trendNews, index } = props;
 	return (
-		<div className={styles.trend} key={index}>
-			<div className={styles.imageStyle}>
-				{
-					(trendNews.jetpack_featured_media_url == "") ? "" : <Image priority loading="eager" layout="fill" src={trendNews.jetpack_featured_media_url} height={209} width={228} alt={trendNews.slug} />
-				}
-			</div>
-			<div className={styles.trendContent}>
-				<span className={styles.date}>{dateFormat(trendNews.date)}</span>
-				<span dangerouslySetInnerHTML={{ __html: trendNews.title.rendered }} className={styles.title}></span>
-				<NewsLink
-				className={styles.headerlink}
-				title={"Read More"}
-				src={trendNews.link}
-				href={trendNews.link}
-				/>
-			</div>
-		</div>
+		<Link href={trendNews.link}>
+			<a target="_blank" rel="norefferer">
+				<div className={styles.trend} key={index}>
+					<div className={styles.imageStyle}>
+						{
+							(trendNews.jetpack_featured_media_url == "") ? "" : <Image priority loading="eager" layout="responsive" src={trendNews.jetpack_featured_media_url} height={209} width={228} alt={trendNews.slug} />
+						}
+					</div>
+					<div className={styles.trendContent}>
+
+						<span dangerouslySetInnerHTML={{ __html: trendNews.title.rendered }} className={styles.title}></span>
+						<span className={styles.date}>{moment(trendNews.date).format("MMM Do, YYYY")}</span>
+					</div>
+				</div>
+			</a>
+		</Link>
 	);
 };
 
