c50afc075160c01989bf5cbf27b276219f92b1cb
mrprotocoll <lekanvgbg@gmail.com>
2023-01-24 07:31:27 +0100

    Add hero and about section of the about us page

--- pages/about/index.js
+++ pages/about/index.js
@@ -13,6 +13,7 @@ export default function Products({ products, teams }) {
         let smallName = name.split(' ')[0].toLowerCase()
         return `/assets/about/team/${smallName}.png`
     }
+    const imagePath = "/assets/about/"
 
     return (
         <div className={styles.aboutUsPage}>
@@ -23,123 +24,56 @@ export default function Products({ products, teams }) {
             <Navigation theme={"dark"} products={products} />
             <main>
                 <section className={styles.welcome}>
-                    <div>
-                        <h1>About Us</h1>
+                    <div className={styles.shape}></div>
+                    <div className={styles.content}>
+                        <div className='container'></div>
+                        <h1>Leaders in Data Migration</h1>
+                        <p>Revolutionizing the Data Migration Industry - Your Partner for Success</p>
                     </div>
                 </section>
-                <section className={styles.ourStory}>
-                    <h2 className={styles.ourStoryTitle} data-aos="fade-down">How it All started</h2>
-                    <div className={styles.ourStoryBody}>
-                        <div className={styles.ourStoryContent}>
-                            <p>
-                                It was the summer of 2016, Taiwo, formerly of the Big Four, had been brought into a large UK Bank digital transformation programme to consolidate their project management tool estate into one consolidated Atlassian toolset, was joined by Wale, also formerly of the Big Four, to establish an Agile Way of Working. The synergy was instant and long lasting.
-                            </p>
-                            <p>
-                                The tools supported the controls, deployment and scaling of the new way of working while the methodology utilised and maximised the Atlassian toolset.
-                            </p>
-                            <p>
-                                We had to consolidate data and configuration from several third-party application, couple of Jira instances to a single new Jira instance. Same for documentation storage and dev ops tools. All the while building, integrating to, and reporting from a new data lake.
-                            </p>
-                            <p>
-                                After the success of that programme, we went to repeat a similar delivery for one of UK's biggest telecom providers.
-                            </p>
-                            <p>
-                                We have since delivered some of the largest Atlassian tools Consolidation and accompanying Administration, Engineering, Integration, and long-term Support of the Atlassian products for all ways of working.
-                            </p>
-                            <p>
-                                We routinely put back into the customers' pockets, 15 – 45% of their annual licence costs, by consolidating and scaling their use of Jira, Confluence, Bitbucket and other Atlassian products, from multiple Atlassian and third-party applications.
-                            </p>
+                <section className={styles.about}>
+                    <div className="container">
+                        <div className="row gap-5">
+                            <div className="col-md-6">
+                                <div className={styles.content}>
+                                    <span>About us</span>
+                                    <h2>About Alluvium</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. </p> 
+                                    <p>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p> <p>Cum sociis natoque penatibus et magnis dis parturient. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient. </p>
+                                </div>
+                            </div>
+                            <div className="position-relative col-md-5">
+                                <Image src={'/assets/about/atlassian-tools-icon.png'} className={styles.image} layout="fill" alt='Atlassian tools'/>
+                            </div>
                         </div>
                     </div>
                 </section>
-                <section className={styles.teamsSection}>
-                    <h2 className={styles.teamsSectionTitle}>
-                        <span>Meet a few friendly faces with </span>
-                        <span>100% dedication to your technological needs and success.</span>
-                    </h2>
-                    <div className={styles.teams}>
-                        <div className={styles.team} data-aos="fade-up">
-                            {
-                                !teams ? (<div>No data</div>)
-                                    :
-                                    (
-                                        teams.map((team, index) => {
-                                            if (index === 0) {
-                                                // return <div className={styles.foundersSection} key={index}>
-                                                //     {
-                                                //         team.map((individual, index2) =>
-                                                //             <Link href={`/about/${individual.url}`} key={index2}>
-                                                //                 <a className='text-white'>
-                                                //                     <div className={styles.founderSection} key={index2}>
-                                                //                         <Image src={getImagePath(individual.name)} width={207.38} height={207.38} alt={individual.name} loading="eager" priority />
-                                                //                         <p>{individual.name}</p>
-                                                //                         <p><b>{individual.role}</b></p>
-                                                //                     </div>
-                                                //                 </a>
-                                                //             </Link>
-                                                //         )
-                                                //     }
-                                                // </div>
-                                                return <span key={index}></span>
-                                            }
-                                            if (index === 1) {
-                                                return <div className={styles.firstStaffsSection} key={index}>
-                                                    {
-                                                        team.map((individual, index2) =>
-                                                            <Link href={`/about/${individual.url}`} key={index2}>
-                                                                <a className={styles.firstStaffSection}>
-                                                                    <Image src={getImagePath(individual.name)} width={207.38} height={(index2 < 4) || (individual.name === 'Adedayo Jesufolakemi') ? 207.38 : 241.38} alt={individual.name} loading="eager" priority />
-                                                                    {
-                                                                        ((index2 < 4) || (individual.name === 'Adedayo Jesufolakemi')) &&
-                                                                        <>
-                                                                            <p>{individual.name}</p>
-                                                                            <p><b>{individual.role}</b></p>
-                                                                        </>
-                                                                    }
-                                                                </a>
-                                                            </Link>
-                                                        )
-                                                    }
-                                                </div>
-                                            }
-                                            if (index === 2) {
-                                                return <div className={styles.firstStaffsSection} key={index}>
-                                                    {
-                                                        team.map((individual, index2) =>
-                                                            <Link href={`/about/${individual.url}`} key={index2}>
-                                                                <a className={styles.firstStaffSection}>
-                                                                    <Image src={getImagePath(individual.name)} width={207.38} height={(individual.name === 'Yemisi Adelowo') ? 207.38 : 241.38} alt={individual.name} loading="eager" priority />
-                                                                    {
-                                                                        (individual.name === 'Yemisi Adelowo') &&
-                                                                        <>
-                                                                            <p>{individual.name}</p>
-                                                                            <p><b>{individual.role}</b></p>
-                                                                        </>
-                                                                    }
-                                                                </a>
-                                                            </Link>)
-                                                    }
-                                                </div>
-                                            }
-                                            if (index === 3) {
-                                                return <div className={styles.firstStaffsSection} key={index}>
-                                                    {
-                                                        team.map((individual, index2) =>
-                                                            <Link href={`/about/${individual.url}`} key={index2}>
-                                                                <a className={styles.firstStaffSection}>
-                                                                    <Image src={getImagePath(individual.name)} width={207.38} height={207.38} alt={individual.name} loading="eager" priority />
 
-                                                                    <p>{individual.name}</p>
-                                                                    <p><b>{individual.role}</b></p>
+                <section className={styles.story}>
+                    <div className="container">
 
-                                                                </a>
-                                                            </Link>)
-                                                    }
-                                                </div>
-                                            }
-                                        })
-                                    )
-                            }
+                        <div className="row">
+                            <div className="col-md-6 position-relative">
+
+                                <div className="position-relative">
+                                    <div className='position-relative'>
+                                        <Image src={imagePath+"Image-2.png"} layout="fill" className={styles.image} />
+                                    </div>
+                                    <div className='position-relative'>
+                                        <Image src={imagePath+"Image-3.png"} layout="fill" className={styles.image} />
+                                    </div>
+                                </div>
+                                
+                                <Image src={imagePath+"Image-1.png"} layout="fill" className={styles.image}/>
+                            </div>
+                            <div className="col-md-6">
+                                <p>It was the summer of 2016, Taiwo, formerly of the Big Four, had been brought into a large UK Bank digital transformation programme to consolidate their project management tool estate into one consolidated Atlassian toolset, was joined by Wale, also formerly of the Big Four, to establish an Agile Way of Working. The synergy was instant and long lasting. </p>
+                                <p><strong>The tools supported the controls, deployment and scaling of the new way of working while the methodology utilised and maximised the Atlassian toolset.</strong> </p>
+                                <p>We had to consolidate data and configuration from several third-party application, couple of Jira instances to a single new Jira instance. Same for documentation storage and Devops tools. All the while building, integrating to, and reporting from a new data lake.</p>
+                                <p>After the success of that programme, we went to repeat a similar delivery for one of UK's biggest telecom providers.
+                                We have since delivered some of the largest Atlassian tools Consolidation and accompanying Administration, Engineering, Integration, and long-term Support of the Atlassian products for all ways of working.</p>
+                                <p><strong>We routinely put back into the customers' pockets, 15 – 45% of their annual license costs,</strong>  by consolidating and scaling their use of Jira, Confluence, Bitbucket and other Atlassian products, from multiple Atlassian and third-party applications.</p>
+                            </div>
                         </div>
                     </div>
                 </section>
