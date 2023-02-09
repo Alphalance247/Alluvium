83cc836e68bb9e6fcb60ceba2243cc44c9c33239
timilxr <38140090+timilxr@users.noreply.github.com>
2023-02-09 21:43:40 +0100

    update: update layout

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AltFooter } from './footer';
 import Navigation from './navigation';
 import styles from '../styles/layout.module.scss';
+import { ConsultationForm } from './Consultation';
 
 export default function Layout ({children}) {
     return (
@@ -10,6 +11,7 @@ export default function Layout ({children}) {
             <div className={styles.layoutBody}>
                 {children}
             </div>
+            <ConsultationForm />
             <AltFooter/>
         </div>  
     )
