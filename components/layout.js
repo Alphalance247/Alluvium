dc5f690bd3bc9f55ae7f851d006f04b73713ed88
timilxr <38140090+timilxr@users.noreply.github.com>
2023-02-09 09:33:38 +0100

    update: added navigation to layout

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AltFooter } from './footer';
+import Navigation from './navigation';
+import styles from '../styles/layout.module.scss';
 
 export default function Layout ({children}) {
     return (
         <div>
-            {children}
+            <Navigation />
+            <div className={styles.layoutBody}>
+                {children}
+            </div>
             <AltFooter/>
         </div>  
     )
