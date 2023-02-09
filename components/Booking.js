2e60db7c6f073530019983349ea684a0518721ca
timilxr <38140090+timilxr@users.noreply.github.com>
2023-02-09 09:30:31 +0100

    feat: make booking dynamic

--- components/Booking.js
+++ components/Booking.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/booking.module.scss';
 
-const BookingCTA = () => {
+const BookingCTA = ({ title, btnText, bgColor, btnClass }) => {
+
+    const [customStyle, setCustomStyle] = useState(null);
+
+    useEffect(() => {
+        if (!bgColor) {
+            setCustomStyle(null);
+        } else {
+            setCustomStyle({ backgroundColor: bgColor });
+        }
+    }, [bgColor]);
+
+
     return (
-        <div className={styles.booking}>
-            <h3>Talk to an Expert About Your Project</h3>
+        <div className={styles.booking} style={customStyle}>
+            <h3>{title ? title : 'Talk to an Expert About Your Project'}</h3>
             <Link href="#consultationForm">
-                <a>Book Consultation</a>
+                <a className={`${styles.button} ${btnClass || styles.defaultButton}`}>{btnText ? btnText : 'Book Consultation'}</a>
             </Link>
         </div>
     )
