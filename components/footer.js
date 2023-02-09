a09f5635412d805bc70ad39e755378155643fd31
timilxr <38140090+timilxr@users.noreply.github.com>
2023-02-09 09:32:29 +0100

    styles: footer code alignment

--- components/footer.js
+++ components/footer.js
@@ -1,143 +1,187 @@
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import Switch from 'react-input-switch';
-import styles from '../styles/footer.module.scss';
-import { useToasts } from 'react-toast-notifications';
-import { ImFacebook, ImLinkedin2 } from 'react-icons/im';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Switch from "react-input-switch";
+import styles from "../styles/footer.module.scss";
+import { useToasts } from "react-toast-notifications";
+import { ImFacebook, ImLinkedin2 } from "react-icons/im";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 export function AltFooter() {
+  const { addToast } = useToasts();
 
-    const { addToast } = useToasts();
+  function submitNewsletterForm(e) {
+    e.preventDefault();
+    addToast("Currently unavailable, Check back later", {
+      appearance: "info",
+      autoDismiss: true,
+    });
+  }
 
-    function submitNewsletterForm(e) {
-        e.preventDefault();
-        addToast("Currently unavailable, Check back later", { appearance: 'info', autoDismiss: true });
-    }
-
-    return (
-        <footer className={styles.altFooter}>
-            <div className={`container ${styles.altFooterMainSection}`}>
-                <div className={styles.altFooterMainSectionComponents}>
-                    <div className={styles.altFooterMainSectionComponent}>
-                        <div className={styles.altFooterLogo}>
-                            <img src={"/assets/alluvium-logo-dark.svg"} alt="Footer logo" />
-                        </div>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit vehicula dui sit amet ligula cursus. dolor sit amet dolor adipiscing..
-                            </p>
-                        </div>
-                        <div>
-                            <h3>DOWNLOAD BROCHURE</h3>
-
-                            <a aria-label='Download our brochure' download='#' className={styles.download}>Download</a>
-                        </div>
-                    </div>
-                    <div className={styles.altFooterMainSectionComponent}>
-                        <h3 className={styles.altFooterMainSectionComponentTitle}>Company</h3>
-                        <ul className={styles.altFooterContactList}>
-                            <li className={styles.contactListItem}>
-                                <Link href="/about">About us</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="#">Leadership</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="https://blog.alluvium.net/">
-                                    <a target="_blank" rel="norefferer">
-                                        Blog
-                                    </a>
-                                </Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="#">
-                                    Success Stories
-                                </Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="/showing-interest">Show interest</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="/showing-interest-list">Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={styles.altFooterMainSectionComponent}>
-                        <h3 className={styles.altFooterMainSectionComponentTitle}>SERVICES</h3>
-                        <ul className={styles.altFooterContactList}>
-                            <li className={styles.contactListItem}>
-                                <Link href="/#services">Atlassian Services</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="/#services">Consulting</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="/#services">Software Development</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={styles.divider}>
-                        <hr />
-                    </div>
-                    <div className={styles.altFooterMainSectionComponent}>
-                        <h3 className={styles.altFooterMainSectionComponentTitle}>SUPPORT</h3>
-                        <ul className={styles.altFooterContactList}>
-                            <li className={styles.contactListItem}>
-                                <Link href="/support">FAQ</Link>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <Link href="/#">Trouble Shooting</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={styles.altFooterMainSectionComponent}>
-                        <h3 className={styles.altFooterMainSectionComponentTitle}>CONTACT US</h3>
-                        <ul className={styles.altFooterContactList}>
-                            <li className={styles.contactListItem}>
-                                <p><b>Headquarter:</b> Kemp House 160, City Road London, EC1V 2NX.</p>
-                                <p><b>Ekiti Branch:</b> No.1 JayTee Ojo Crescent, Off Okemesi Road, Ikoro Ekiti, Ekiti State.</p>
-                            </li>
-                            <li className={styles.contactListItem}>
-                                <b>Email:</b>
-                                <a href="mailto:contact@alluvium.net"> contact@alluvium.net</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-md-5'>
-                        <hr />
-                    </div>
-                    <div className='col-md-2 px-4 mx-md-auto d-flex justify-content-between align-items-center'>
-                        <a href="https://www.facebook.com/alluviumhq/?ti=as" aria-label='facebook' target="_blank">
-                            <ImFacebook />
-                        </a>
-                        {/* <AiFillInstagram /> */}
-                        <a href="https://twitter.com/alluviumhq?s=08" aria-label='twitter' target="_blank">
-                            <AiOutlineTwitter />
-                        </a>
-                        <a href="https://www.linkedin.com/company/alluvium-hq/" aria-label='linkedIn' target="_blank">
-                            <ImLinkedin2 />
-                        </a>
-                    </div>
-                    <div className='col-md-5'>
-                        <hr />
-                    </div>
-
-                </div>
-                <div className='d-md-flex justify-content-between mt-4'>
-                    <div className={styles.copyrights}>
-                        <p>© {new Date().getFullYear()} Alluvium Corporation. All rights reserved.</p>
-                    </div>
-                    <div className={styles.extralinks}>
-                        <a href='#' className='me-3'>Cookie Settings</a>
-                        <a href='#' className='me-3'>Legal Information</a>
-                        <a href='#'>Privacy Policy</a>
-                    </div>
-                </div>
+  return (
+    <footer className={styles.altFooter}>
+      <div className={`container ${styles.altFooterMainSection}`}>
+        <div className={styles.altFooterMainSectionComponents}>
+          <div className={styles.altFooterMainSectionComponent}>
+            <div className={styles.altFooterLogo}>
+              <img src={"/assets/alluvium-logo-dark.svg"} alt="Footer logo" />
             </div>
-        </footer>
-    )
+            <div>
+              <p>
+                We are a team of passionate professionals dedicated to providing
+                businesses with cutting-edge solutions leveraging the Atlassian
+                suite of tools.
+              </p>
+            </div>
+            <div>
+              <h3>DOWNLOAD BROCHURE</h3>
+
+              <a
+                aria-label="Download our brochure"
+                download="#"
+                className={styles.download}
+              >
+                Download
+              </a>
+            </div>
+          </div>
+          <div className={styles.altFooterMainSectionComponent}>
+            <h3 className={styles.altFooterMainSectionComponentTitle}>
+              Company
+            </h3>
+            <ul className={styles.altFooterContactList}>
+              <li className={styles.contactListItem}>
+                <Link href="/about">About us</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="#">Leadership</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="https://blog.alluvium.net/">
+                  <a target="_blank" rel="norefferer">
+                    Blog
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="#">Success Stories</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="/showing-interest">Show interest</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="/showing-interest-list">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.altFooterMainSectionComponent}>
+            <h3 className={styles.altFooterMainSectionComponentTitle}>
+              SERVICES
+            </h3>
+            <ul className={styles.altFooterContactList}>
+              <li className={styles.contactListItem}>
+                <Link href="/#services">Atlassian Services</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="/#services">Consulting</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="/#services">Software Development</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.divider}>
+            <hr />
+          </div>
+          <div className={styles.altFooterMainSectionComponent}>
+            <h3 className={styles.altFooterMainSectionComponentTitle}>
+              SUPPORT
+            </h3>
+            <ul className={styles.altFooterContactList}>
+              <li className={styles.contactListItem}>
+                <Link href="/support">FAQ</Link>
+              </li>
+              <li className={styles.contactListItem}>
+                <Link href="/#">Trouble Shooting</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.altFooterMainSectionComponent}>
+            <h3 className={styles.altFooterMainSectionComponentTitle}>
+              CONTACT US
+            </h3>
+            <ul className={styles.altFooterContactList}>
+              <li className={styles.contactListItem}>
+                <p>
+                  <b>Headquarter:</b> Kemp House 160, City Road London, EC1V
+                  2NX.
+                </p>
+                <p>
+                  <b>Ekiti Branch:</b> No.1 JayTee Ojo Crescent, Off Okemesi
+                  Road, Ikoro Ekiti, Ekiti State.
+                </p>
+              </li>
+              <li className={styles.contactListItem}>
+                <b>Email:</b>
+                <a href="mailto:contact@alluvium.net"> contact@alluvium.net</a>
+              </li>
+              <li className={styles.contactListItem}>
+                <b>Phone Number:</b>
+                <a href="tel:+234 812 345 6789"> +234 812 345 6789</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-5">
+            <hr />
+          </div>
+          <div className="col-md-2 px-4 mx-md-auto d-flex justify-content-between align-items-center">
+            <a
+              href="https://www.facebook.com/alluviumhq/?ti=as"
+              aria-label="facebook"
+              target="_blank"
+            >
+              <ImFacebook />
+            </a>
+            {/* <AiFillInstagram /> */}
+            <a
+              href="https://twitter.com/alluviumhq?s=08"
+              aria-label="twitter"
+              target="_blank"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/alluvium-hq/"
+              aria-label="linkedIn"
+              target="_blank"
+            >
+              <ImLinkedin2 />
+            </a>
+          </div>
+          <div className="col-md-5">
+            <hr />
+          </div>
+        </div>
+        <div className="d-md-flex justify-content-between mt-4">
+          <div className={styles.copyrights}>
+            <p>
+              © {new Date().getFullYear()} Alluvium Corporation. All rights
+              reserved.
+            </p>
+          </div>
+          <div className={styles.extralinks}>
+            <a href="#" className="me-3">
+              Cookie Settings
+            </a>
+            <a href="#" className="me-3">
+              Legal Information
+            </a>
+            <a href="#">Privacy Policy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
