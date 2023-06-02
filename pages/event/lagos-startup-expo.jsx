c6c66adf364284393b1c0ad3974086b4d1879a32
timilxr <38140090+timilxr@users.noreply.github.com>
2023-06-02 13:15:38 +0100

    Page Description update
    Page Description update

--- pages/event/lagos-startup-expo.jsx
+++ pages/event/lagos-startup-expo.jsx
@@ -44,15 +44,15 @@ const LagosStartUpExpoPage = () => {
             <ToastProvider>
                 <Layout withoutForm={true}>
                     <Head>
-                        <title>Home | Alluvium</title>
+                        <title>Lagos Startup Expo | Alluvium</title>
                         <link rel="icon" href="/favicon.ico" />
                         <meta
                             name="description"
-                            content="Alluvium is an Atlassian Products Migration Lab. We produce Tools, Systems and Services that delivers complete data migration in half the time for half the cost."
+                            content="Alluvium and over 200 startups and tech firms converged on the Landmark Event Centre in Victoria Island, Lagos Nigeria over the weekend to discuss a push for Africa and Nigeria in the Fourth Industrial revolution at the Lagos Startup Expo."
                         />
                         <meta
                             name="keywords"
-                            content="Alluvium, alluvium, team alluvium, atlassian products migration lab, migration, about alluvium, alluvians, cloud counter, Migration Experts, Software Consulting atlassian, confluence, jira"
+                            content="Alluvium, alluvium, Techpoint startup, team alluvium, atlassian products migration lab, migration, about alluvium, alluvians, cloud counter, Migration Experts, Software Consulting atlassian, confluence, jira"
                         />
                     </Head>
                     <div className={`container-fluid p-0 m-0 ${styles.main}`}>
