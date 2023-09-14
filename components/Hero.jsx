2a566e5e682fe6a61d7aa7b94a365aa9792c87b4
Yusuf Braimoh <73256563+Yusuf-TJ@users.noreply.github.com>
2023-09-14 22:19:54 +0100

    aws badges update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,7 +22,9 @@ const Hero = () => {
                   <div className={styles.badges_cover}>
                     <div className={styles.badges}>
                       <Image src={"/assets/atlassian-badges/market-place-white.png"} loading="eager" priority width={400} height={138} alt={"Atlassian Platinum Market Place Partner"} />
-                      <Image src={"/assets/atlassian-badges/aws-cloud.png"} loading="eager" priority width={198} height={198} alt={"AWS Certified Cloud Practitioner"} />
+                      {/* <Image src={"/assets/atlassian-badges/aws-cloud.png"} loading="eager" priority width={198} height={198} alt={"AWS Certified Cloud Practitioner"} /> */}
+                      <Image src={"/assets/atlassian-badges/aws-partner.png"} loading="eager" priority width={198} height={198} alt={"AWS Partner"} />
+                      <Image src={"/assets/atlassian-badges/aws-qualified-software-partner.png"} loading="eager" priority width={198} height={198} alt={"AWS Qualified Software Partner"} />
                       <Image src={"/assets/atlassian-badges/atl-expert.png"} className={styles.stroke} loading="eager" priority width={198} height={198} alt={"Atlassian Certified Expert"} />
                       <Image src={"/assets/atlassian-badges/gcp-02.jpeg"} className={styles.stroke} loading="eager" priority width={198} height={198} alt={"Google Cloud Certified, Cloud Digital Leader"} />
                       <Image src={"/assets/atlassian-badges/atl.png"} className={styles.stroke} loading="eager" priority width={198} height={198} alt={"Atlassian Certified Jira Administrator for Cloud"} />
