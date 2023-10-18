ccee2d8b0ea13015fc2537e684310ead2261964a
Sherif Siyanbola <sherifsiyanbola@gmail.com>
2023-10-18 15:32:25 +0100

    Added the count down section

---
+++ components/CountdownTimer.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+const CountdownTimer = ({ targetDate }) => {
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining(targetDate));
+
+  function calculateTimeRemaining(targetDate) {
+    const now = new Date();
+    const targetTime = new Date(targetDate);
+    const timeDiff = targetTime - now;
+
+    if (timeDiff <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining(targetDate));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
+
+  const countdownStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  const unitStyles = {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    margin: '0 10px', // Adjust the spacing between units as needed
+  };
+
+  return (
+    <div style={countdownStyles}>
+      <span style={unitStyles}>{timeRemaining.days}</span> days
+      <span style={unitStyles}>{timeRemaining.hours}</span> hours
+      <span style={unitStyles}>{timeRemaining.minutes}</span> minutes
+      <span style={unitStyles}>{timeRemaining.seconds}</span> seconds
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- components/EventHero.jsx
+++ components/EventHero.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/eventpage.module.scss";
-
+import CountdownTimer from './CountdownTimer';
 
 // import cloud_image from 'public/assets/cloud_image.jpg';
 // import event_header_image from 'public/assets/event_header_image';
@@ -21,8 +21,10 @@ const EventHero = () => {
               alt="Image 1"
             />
             <div className="card-body">
-              <h5 className="card-title">Event Image 1</h5>
-              <p className="card-text">Description of the event.</p>
+              <h5 className="card-title text-warning">ONLINE & IN-PERSON</h5>
+              <p className="event-desc">Harnessing global synergy to forge collaboration for digital<br></br>
+              opportunities
+              </p>
               <a href="#" className="btn btn-warning">REGISTER</a>
             </div>
           </div>
@@ -37,8 +39,6 @@ const EventHero = () => {
               alt="Image 2"
             />
             <div className="card-body">
-              <h5 className="card-title">Event Image 2</h5>
-              <p className="card-text">Description of the event.</p>
             </div>
           </div>
         </div>
@@ -47,9 +47,22 @@ const EventHero = () => {
 
     </div>
 
-    <div>
+    <div className="bg-warning">
+    <section className="container">
+      <div className="row">
+        {/* First Card */}
+        <div className="col-md-6 mb-4">
+        <CountdownTimer targetDate="2023-12-31T23:59:59" />
+        </div>
 
-        here we go
+        {/* Second Card */}
+        <div className="col-md-6 mb-4">
+          {/* ... (your card code) */}
+        </div>
+      </div>
+    </section>
+      <hr className={styles.hr} />
+      gdyiwyg
     </div>
     </>
   );
