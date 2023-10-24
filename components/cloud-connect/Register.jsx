0633bf886e6b690db9a154c8f00d6d4473f6494c
Alphalance247 <al.moshood017@gmail.com>
2023-10-24 18:44:48 +0100

    changesMade

--- components/cloud-connect/Register.jsx
+++ components/cloud-connect/Register.jsx
@@ -4,7 +4,7 @@ const Register = () => {
   return (
     <div className={styles.register}>
       <h1>Registration is now open for onsite and virtual attendees</h1>
-      <button>Buy Tickets</button>
+      <button className={styles.btn}>Buy Tickets</button>
     </div>
   );
 };
