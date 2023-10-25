0ea82cac82ba21a7b7799284c6f29054d1a9b30b
timilxr <38140090+timilxr@users.noreply.github.com>
2023-10-25 14:24:17 +0100

    renamed eventpage

---
+++ pages/event/cloud-connect.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Layout from "components/layout";
+import EventHero from "components/cloud-connect/EventHero";
+import Register from "components/cloud-connect/Register";
+import Schedule from "components/cloud-connect/Schedule";
+import Speaker from "components/cloud-connect/Speaker";
+
+const CloudConnect = () => {
+  return (
+    <>
+      <Layout>
+        <EventHero />
+        <Schedule />
+        <Speaker />
+        <Register />
+      </Layout>
+    </>
+  );
+};
+
+export default CloudConnect;
