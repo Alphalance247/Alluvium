783bced856d12ad15837748d7c2b2450df874804
timilxr <38140090+timilxr@users.noreply.github.com>
2023-11-02 20:29:05 +0100

    Speaker fix

--- components/cloud-connect/Speaker.jsx
+++ components/cloud-connect/Speaker.jsx
@@ -2,64 +2,80 @@ import styles from "../../styles/eventpage.module.scss";
 
 const Speaker = () => {
   const guestData = [
-    {
-      id: 1,
-      name: "Tala Saleh",
-      content:
-        "Atlassian Partner Manager| Southern Europe, Middle East, Turkey and Africa",
-      src: "/cloudImg/mine1.jpg",
-      alt: "Tala Saleh",
-    },
-    {
-      id: 2,
-      name: "Tyler Venable",
-      content: "Atlas Bench Head of Operations and Talent Solutions",
-      src: "/cloudImg/mine2.png",
-      alt: "Tyler Venable",
-    },
-    {
-      id: 3,
-      name: "Mat May",
-      content: "E7 Solutions, LLC, System Administrator",
-      src: "/cloudImg/mine3.png",
-      alt: "Mat May",
-    },
-    {
-      id: 4,
-      name: "Ted Henry",
-      content: "Principal Atlassian Solutions Architect at Forty8Fifty Labs",
-      src: "/cloudImg/mine4.png",
-      alt: "Ted Henry",
-    },
-    {
-      id: 5,
-      name: "Katakyie Kojo Desu",
-      content: "Business Development Manager at On Point, Africa",
-      src: "/cloudImg/mine5.jpg",
-      alt: "Katakyie Kojo Desu",
-    },
+    // {
+    //   id: 1,
+    //   name: "Tala Saleh",
+    //   content:
+    //     "Atlassian Partner Manager| Southern Europe, Middle East, Turkey and Africa",
+    //   src: "/cloudImg/mine1.jpg",
+    //   alt: "Tala Saleh",
+    // },
+    // {
+    //   id: 2,
+    //   name: "Tyler Venable",
+    //   content: "Atlas Bench Head of Operations and Talent Solutions",
+    //   src: "/cloudImg/mine2.png",
+    //   alt: "Tyler Venable",
+    // },
+    // {
+    //   id: 3,
+    //   name: "Mat May",
+    //   content: "E7 Solutions, LLC, System Administrator",
+    //   src: "/cloudImg/mine3.png",
+    //   alt: "Mat May",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Ted Henry",
+    //   content: "Principal Atlassian Solutions Architect at Forty8Fifty Labs",
+    //   src: "/cloudImg/mine4.png",
+    //   alt: "Ted Henry",
+    // },
+    // {
+    //   id: 5,
+    //   name: "Katakyie Kojo Desu",
+    //   content: "Business Development Manager at On Point, Africa",
+    //   src: "/cloudImg/mine5.jpg",
+    //   alt: "Katakyie Kojo Desu",
+    // },
     {
       id: 6,
       name: "Erica Larson",
       content:
         "People/Project/Process Leader, Atlassian Cloud Administrator, Atlassian Community Leader",
-      src: "/cloudImg/mine6.jpg",
+      src: "/cloudImg/6.png",
       alt: "Erica Larson",
     },
+    // {
+    //   id: 7,
+    //   name: "Margaret Wang",
+    //   content: "Dedicated to Sustainable Development through Education",
+    //   src: "/cloudImg/mine7.jpg",
+    //   alt: "Margaret Wang",
+    // },
+    // {
+    //   id: 8,
+    //   name: "Oluwaseun Fakuade",
+    //   content:
+    //     "Hon. Commissioner for Innovation Science and Digital Economy, Ekiti State",
+    //   src: "/cloudImg/mine8.jpg",
+    //   alt: "Oluwaseun Fakuade",
+    // },
     {
-      id: 7,
-      name: "Margaret Wang",
-      content: "Dedicated to Sustainable Development through Education",
-      src: "/cloudImg/mine7.jpg",
-      alt: "Margaret Wang",
+      id: 9,
+      name: "Samuel Desu Acheampong",
+      content:
+        "Samuel Kojo Desu Acheampong currently serves as the sales team lead for onpoint Africa.",
+      src: "/cloudImg/9.png",
+      alt: "Samuel Desu Acheampong",
     },
     {
-      id: 8,
-      name: "Oluwaseun Fakuade",
+      id: 10,
+      name: "Olaniyan Remilekun",
       content:
-        "Hon. Commissioner for Innovation Science and Digital Economy, Ekiti State",
-      src: "/cloudImg/mine8.jpg",
-      alt: "Oluwaseun Fakuade",
+        "Olaniyan Remilekun is an Artificial Intelligence Engineer and Data Scientist.",
+      src: "/cloudImg/10.png",
+      alt: "Olaniyan Remilekun",
     },
   ];
 
@@ -73,7 +89,7 @@ const Speaker = () => {
               <div
                 className={styles.specific1}
                 key={guest.id}
-                style={{ backgroundImage: `url(${guest.src})` }}
+                style={{ backgroundImage: `url(${guest.src})`, backgroundPosition: `top`}}
               >
                 <h3 className={styles.specific}>{guest.name}</h3>
                 <p className={styles.specific}>{guest.content}</p>
