e01b86c24c9c0c4e1d1a3127a30c99d6e98ac9ad
timilxr <38140090+timilxr@users.noreply.github.com>
2023-11-05 00:18:45 +0100

    Fix

--- components/cloud-connect/EventHero.jsx
+++ components/cloud-connect/EventHero.jsx
@@ -68,7 +68,7 @@ const EventHero = () => {
                 width={"20px"}
                 height={"20px"}
               />
-              <strong>Location:</strong> <br /> Arowolo Gardens,  adjacent fajuyi Pavillion, home to Ayoba Fm ,beside Ekiti state fire service.  Fajuyi Ado Ekiti.
+              <strong>Location:</strong> <br /> Arowolo Gardens,  adjacent Fajuyi Pavillion, home to Ayoba FM, beside Ekiti State Fire Service.  Fajuyi Ado Ekiti.
             </p>
           </div>
           <div className="col-md-3">
