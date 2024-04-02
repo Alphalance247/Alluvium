98cf6fd4a4c96c04bc3cee0ea93162e3b5e52c6e
timilxr <38140090+timilxr@users.noreply.github.com>
2024-04-02 19:33:12 +0100

    remove cloud connect banner

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -13,9 +13,9 @@ export default function () {
         <>
             <div className="container-fluid p-0">
                 <Slider speed={3000} autoplaySpeed={7000} arrows={false} dots={false} autoplay={true} pauseOnHover={false} pauseOnFocus={false} infinite={true} swipe >
-                    <div>
+                    {/* <div>
                         <EventHero showCountdown={false} />
-                    </div>
+                    </div> */}
                     <div>
                         <Hero />
                     </div>
