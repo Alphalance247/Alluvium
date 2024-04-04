d6970a380998f831d59ee50721631da6dcf9a31e
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-04-04 10:09:42 +0100

    Updated U.I

--- components/TEAM24/Hero.jsx
+++ components/TEAM24/Hero.jsx
@@ -19,14 +19,15 @@ const Hero = ({}) => {
           }}
         >
           <div
-            className={`container mx-auto position-relative ${styles.centercontent}`}
+            className={`container-xxl mx-auto position-relative ${styles.centercontent}`}
           >
             <div
-              className={`container mx-auto ${styles.content}`}
+              className={`container-fluid mx-auto ${styles.content}`}
               style={{ transform: " translate(-50%, -50%)" }}
             >
+              <p className={styles.withlove}>From Africa with Love</p>
               <h1 className={styles.heading}>
-                Get Ready for Alluvium's Spectacular Presence at
+                Get ready for an unforgettable moment at
               </h1>
               <div className={styles.imageBorders}>
                 <Image
@@ -37,7 +38,7 @@ const Hero = ({}) => {
                   className={styles.imageBorder}
                 />
               </div>
-              <p>
+              <p className={styles.eventdate}>
                 APRIL 30 - MAY 2 <span>|</span> Las Vegas and digitally
               </p>
             </div>
@@ -59,7 +60,7 @@ const Hero = ({}) => {
                 className={styles.menCap}
               />
             </div>
-            <div
+            {/* <div
               style={{ position: "absolute", top: "0", left: "17rem" }}
               className={styles.curveright}
             >
@@ -80,7 +81,7 @@ const Hero = ({}) => {
                 alt=""
                 className={styles.curve}
               />
-            </div>
+            </div> */}
           </div>
         </div>
         <div
@@ -89,27 +90,30 @@ const Hero = ({}) => {
         ></div>
       </section>
       <section className={styles.section2}>
-        <h2>From Africa with Love</h2>
-        <p>
-          Experience the love from Africa as we unite to celebrate the power of
-          teamwork and innovation at Vegas on April 30th - May 2nd. Discover our
-          exceptional Atlassian solutions firsthand and engage with our
-          passionate team, who will be ready to share insights, provide expert
-          guidance, and help you up your team game. Join us at team‘24 where
-          cultures converge and ideas flourish. Together, let's create a
-          memorable event filled with meaningful connections, transformative
-          experiences, and a shared vision for a brighter future. We can't wait
-          to meet you at stand 53 and present to you our exclusive souvenirs as
-          a special gesture from Alluvium. See you there!
-        </p>
-        <div>
-          <img
-            src="/assets/team2024/Frame.png"
-            width={415}
-            height={69}
-            alt="frames"
-            className={styles.img}
-          />
+        <div className="container mx-auto">
+          <h2>From Africa with Love</h2>
+          <p>
+            Experience the love from Africa as we unite to celebrate the power
+            of teamwork and innovation at Vegas on April 30th - May 2nd.
+            Discover our exceptional Atlassian solutions firsthand and engage
+            with our passionate team, who will be ready to share insights,
+            provide expert guidance, and help you up your team game. Join us at
+            team‘24 where cultures converge and ideas flourish. Together, let's
+            create a memorable event filled with meaningful connections,
+            transformative experiences, and a shared vision for a brighter
+            future. We can't wait to meet you at stand 53 and present to you our
+            exclusive souvenirs as a special gesture from Alluvium. See you
+            there!
+          </p>
+          <div>
+            <img
+              src="/assets/team2024/Frame.png"
+              width={415}
+              height={69}
+              alt="frames"
+              className={styles.img}
+            />
+          </div>
         </div>
       </section>
     </>
