cf8a15957c80b3ff3d64fd5b2a06065fcf21ed5c
timilxr <38140090+timilxr@users.noreply.github.com>
2024-04-29 15:11:51 +0100

    content update

--- pages/boot53/index.js
+++ pages/boot53/index.js
@@ -11,8 +11,16 @@ export default function Boot53() {
     <Layout>
       <div>
         <Head>
-          <title>Team | Alluvium</title>
+        <title>Booth 53 | Alluvium</title>
           <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Welcome to booth 53, Unlock full potential of your Atlassian products. Experience Alluvium’s presence at Team ‘24, a remarkable event celebrating the synergy of teamwork and innovation. Join us in Las Vegas or digitally from April 30th to May 2nd to explore our outstanding Atlassian solutions, connect with our dedicated team, and inspire life-changing ideas. Register now to secure your exclusive African-inspired souvenir."
+          />
+          <meta
+            name="keywords"
+            content="Alluvium, Booth 53, 53, Alluvium Booth, Team '24', Atlassian, Teamwork and Innovation, Las Vegas, Digital Event, Atlassian Solutions, Dedicated Team, Africa, Africa at Team ‘24, Alluvium at Team ‘24, African-inspired Souvenir, Collaboration, Growth, Networking, Technology Conference, Professional Development"
+          />
         </Head>
       </div>
 
