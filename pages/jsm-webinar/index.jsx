75861056d4cdb25dbdce6259ad15fc84e22aa9f6
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-05-31 15:56:49 +0100

    jsmbinar

---
+++ pages/jsm-webinar/index.jsx
@@ -0,0 +1,13 @@
+const PluginPage = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh", overflowX: "hidden" }}>
+      <iframe
+        src="https://sites.ziftsolutions.com/atlassian.ziftsolutions.com/8a9983108fc086ab018fc109022335b8"
+        style={{ border: "none", width: "100%", height: "100%" }}
+        title="Plugin"
+      />
+    </div>
+  );
+};
+
+export default PluginPage;
