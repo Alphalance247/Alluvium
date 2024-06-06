103540fa3288b646bb471751c4aac17fc002a6df
geekmsd <sherifsiyanbola@gmail.com>
2024-06-06 15:18:36 +0100

    Update webinar.js

--- components/webinar/webinar.js
+++ components/webinar/webinar.js
@@ -15,13 +15,15 @@ const Webinar = () => {
         <div className={`row container mx-auto ${styles.visitbanner}`}>
 
           <div className="col-md-6">
+            <div className="" style={{paddingTop:"65px"}}>
           <h1 className="text-white">Enhance Your Jira Service Management Skills!</h1>
-            <p className="text-white">Join our webinar for exclusive Atlassian lightning talks. Gain in-depth insights, expert tips, live demos, and more. Click here to learn more!</p>
+            <p className="text-white" style={{fontSize:"16.7px"}}>Join our webinar for exclusive Atlassian lightning talks. Gain in-depth insights, expert tips, live demos, and more. Click here to learn more!</p>
             <Link href={'/'}>
-              <a className="btn btn-warning">
+              <a className="btn text-white" style={{backgroundColor:"#E37915"}}>
                 Watch webinars
               </a>
             </Link>
+            </div>
           </div>
           <div className="col-md-6">
           <Image width={479} height={339} src="/assets/jiraimg.png" />
