a0cbbd240b0480cfee7761882dfca2d66e8ac628
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-06-13 14:32:36 +0100

    changes in checkbox

--- components/Consultation.js
+++ components/Consultation.js
@@ -1,165 +1,257 @@
-import React, { useState } from 'react';
-import { ToastProvider, useToasts } from 'react-toast-notifications';
-import { validateEmail } from 'lib/validation';
-import Link from 'next/link';
-import styles from '../styles/booking.module.scss';
+import React, { useState } from "react";
+import { ToastProvider, useToasts } from "react-toast-notifications";
+import { validateEmail } from "lib/validation";
+import Link from "next/link";
+import styles from "../styles/booking.module.scss";
 
 export const BookingCTA = () => {
-    return (
-        <div className={styles.booking}>
-            <h3>Talk to an Expert About Your Project</h3>
-            <Link href="#consultationForm">
-                <a className={`${styles.button} ${styles.defaultButton}`}>Book Consultation</a>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className={styles.booking}>
+      <h3>Talk to an Expert About Your Project</h3>
+      <Link href="#consultationForm">
+        <a className={`${styles.button} ${styles.defaultButton}`}>
+          Book Consultation
+        </a>
+      </Link>
+    </div>
+  );
+};
 
 export const ConsultationForm = () => {
-    const [fullname, setFullname] = useState('');
-    const [email, setEmail] = useState('');
-    const [company, setCompany] = useState('');
-    const [phone, setPhone] = useState('');
-    const [message, setMessage] = useState('');
-    const [isChecked, setIsChecked] = useState(false);
-    const { addToast } = useToasts();
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [company, setCompany] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const { addToast } = useToasts();
 
-    const onsubmit = async (e) => {
-        e.preventDefault();
-        if (!fullname || !email || !phone || !message) {
-            addToast("All fields marked with asterisks are required", { appearance: 'error' });
-            return;
-        }
-        if (!validateEmail(email)) {
-            addToast("Please enter a valid email address", { appearance: 'error' });
-            return;
-        }
-        if (!isChecked) {
-            addToast("You must agree to the terms and conditions", { appearance: 'error' });
-            return;
-        }
-
-        let formData = {
-            fullname: fullname,
-            email: email,
-            company: company,
-            phone_number: phone,
-            how_we_can_help: message,
-        };
-
-        try {
-            let response = await fetch("https://vast.ec2.alluvium.net/support/inquiry/", {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData),
-            });
-
-            let result = await response.json();
-            const status = result.status;
-            console.log(result);
-            // show message based on status
-            addToast(result.success, { appearance: status });
-        } catch (e) {
-            console.log(e.message);
-            addToast('Oops something went wrong. Please try again.', { appearance: "error" });
-        }
-
-        setFullname("");
-        setEmail("");
-        setPhone("");
-        setCompany("");
-        setMessage("");
-        setIsChecked(false);
+  const onsubmit = async (e) => {
+    e.preventDefault();
+    if (!fullname || !email || !phone || !message) {
+      addToast("All fields marked with asterisks are required", {
+        appearance: "error",
+      });
+      return;
+    }
+    if (!validateEmail(email)) {
+      addToast("Please enter a valid email address", { appearance: "error" });
+      return;
+    }
+    if (!isChecked) {
+      addToast("You must agree to the terms and conditions", {
+        appearance: "error",
+      });
+      return;
     }
 
-    return (
-        <ToastProvider>
-            <section id='consultationForm' className={styles.contact}>
-                <div className="container">
-                    <h2>How can we help?</h2>
-                    <div className={styles.content}>
-                        <div>
-                            <form method='POST' onSubmit={onsubmit}>
-                                <div className="row my-4">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="fullname">Full Name <span className='text-danger'>*</span></label>
-                                        <input onChange={(e) => setFullname(e.target.value)} value={fullname} type="text" className="form-control" name="fullname" id='fullname' placeholder="" />
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="email">Email <span className='text-danger'>*</span></label>
-                                        <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="form-control" name="email" id='email' placeholder="" />
-                                    </div>
-                                </div>
+    let formData = {
+      fullname: fullname,
+      email: email,
+      company: company,
+      phone_number: phone,
+      how_we_can_help: message,
+    };
 
-                                <div className="row my-4">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="company">Company <span className='text-danger'>*</span></label>
-                                        <input onChange={(e) => setCompany(e.target.value)} value={company} type="text" className="form-control" name="company" id='company' placeholder="" />
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="telephone">Phone <span className='text-danger'>*</span></label>
-                                        <input onChange={(e) => setPhone(e.target.value)} value={phone} type="text" className="form-control" name="telephone" id='telephone' placeholder="" />
-                                    </div>
-                                </div>
+    try {
+      let response = await fetch(
+        "https://vast.ec2.alluvium.net/support/inquiry/",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
-                                <div className="form-group col-md-12">
-                                    <label htmlFor="message">How can we help you? <span className='text-danger'>*</span></label>
-                                    <textarea onChange={(e) => setMessage(e.target.value)} value={message} name="message" id="message" className='form-control' rows="7"></textarea>
-                                </div>
+      let result = await response.json();
+      const status = result.status;
+      console.log(result);
+      // show message based on status
+      addToast(result.success, { appearance: status });
+    } catch (e) {
+      console.log(e.message);
+      addToast("Oops something went wrong. Please try again.", {
+        appearance: "error",
+      });
+    }
 
-                                <div className="form-group col-md-12">
-                                    <input
-                                        type="checkbox"
-                                        id="consent"
-                                        name="consent"
-                                        className="mr-2 leading-tight"
-                                        checked={isChecked}
-                                        onChange={(e) => setIsChecked(e.target.checked)}
-                                        required
-                                    />
-                                    <label htmlFor="consent">Please be aware that the information you provide will be used in our website, news letter, and other marketing materials. <span className='text-danger'>*</span></label>
-                                </div>
+    setFullname("");
+    setEmail("");
+    setPhone("");
+    setCompany("");
+    setMessage("");
+    setIsChecked(false);
+  };
 
-                                <div className="form-group col-md-12 mt-3 border border-info">
-                                    <label htmlFor="consentchangemind">If you change your mind in the future, you can email us at contact@alluvium.net to withdraw your consent. <span className='text-default'></span></label>
-                                </div>
-
-                                <div className="form-group mt-5">
-                                    <button type="submit" aria-label='submit' className={`${styles.button} ${styles.defaultButton} ${styles.consultButton}`} disabled={!isChecked}>Submit</button>
-                                </div>
-
-                            </form>
-                        </div>
-                        <div>
-                            <h3>Our Locations</h3>
-                            <p>
-                                <strong>Europe:</strong> Kemp House 160, City Road London, EC1V
-                                2NX, UK.
-                            </p>
-                            <p>
-                                <strong>America:</strong> 27345 W Sand Lake Rd,
-                  STE 210 Office 3190
-                  Orlando, FL 32819 US.
-                            </p>
-                            <div>
-                                <strong>Africa:</strong>
-                                <p>
-                                    Lagos: Dominion Plaza, 140/141 Lekki - Epe Expressway, Lekki, Lagos 106104, Nigeria.
-                                </p>
-                                <p>
-                                    Ekiti: No.1 JayTee Ojo Crescent, Off Okemesi
-                                    Road, Ikoro Ekiti, Ekiti State, Nigeria.
-                                </p>
-                            </div>
-                            <p><strong>Email:</strong> <a href="mailto:contact@alluvium.net">contact@alluvium.net</a> </p>
-                            <p><strong>Phone Number:</strong> <a href="tel:+442035762028"> +442035762028</a> </p>
-                        </div>
-                    </div>
+  return (
+    <ToastProvider>
+      <section id="consultationForm" className={styles.contact}>
+        <div className="container">
+          <h2>How can we help?</h2>
+          <div className={styles.content}>
+            <div>
+              <form method="POST" onSubmit={onsubmit}>
+                <div className="row my-4">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="fullname">
+                      Full Name <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      onChange={(e) => setFullname(e.target.value)}
+                      value={fullname}
+                      type="text"
+                      className="form-control"
+                      name="fullname"
+                      id="fullname"
+                      placeholder=""
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="email">
+                      Email <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      onChange={(e) => setEmail(e.target.value)}
+                      value={email}
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      placeholder=""
+                    />
+                  </div>
                 </div>
-            </section>
-        </ToastProvider>
-    )
-}
+
+                <div className="row my-4">
+                  <div className="form-group col-md-6">
+                    <label htmlFor="company">
+                      Company <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      onChange={(e) => setCompany(e.target.value)}
+                      value={company}
+                      type="text"
+                      className="form-control"
+                      name="company"
+                      id="company"
+                      placeholder=""
+                    />
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label htmlFor="telephone">
+                      Phone <span className="text-danger">*</span>
+                    </label>
+                    <input
+                      onChange={(e) => setPhone(e.target.value)}
+                      value={phone}
+                      type="text"
+                      className="form-control"
+                      name="telephone"
+                      id="telephone"
+                      placeholder=""
+                    />
+                  </div>
+                </div>
+
+                <div className="form-group col-md-12">
+                  <label htmlFor="message">
+                    How can we help you? <span className="text-danger">*</span>
+                  </label>
+                  <textarea
+                    onChange={(e) => setMessage(e.target.value)}
+                    value={message}
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="7"
+                  ></textarea>
+                </div>
+
+                <div
+                  className=""
+                  style={{
+                    display: "flex",
+                    marginTop: "3rem",
+                    columnGap: "1.5rem",
+                  }}
+                >
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="consent"
+                      name="consent"
+                      className="mr-2 leading-tight"
+                      checked={isChecked}
+                      onChange={(e) => setIsChecked(e.target.checked)}
+                      required
+                      style={{ transform: "scale(1.5)" }}
+                    />
+                  </div>
+                  <div className="" style={{ marginTop: "1.3rem" }}>
+                    <label htmlFor="consent">
+                      Please be aware that the information you provide will be
+                      used in our website, news letter, and other marketing
+                      materials. <br /> <br />
+                      <span className="text-danger">*</span> If you change your
+                      mind in the future, you can email us at
+                      <span>
+                        <a href="mailto:contact@alluvium.net">
+                          {` contact@alluvium.net `}
+                        </a>
+                      </span>
+                      to withdraw your consent.
+                    </label>
+                  </div>
+                </div>
+
+                <div className="form-group mt-5">
+                  <button
+                    type="submit"
+                    aria-label="submit"
+                    className={`${styles.button} ${styles.defaultButton} ${styles.consultButton}`}
+                    disabled={!isChecked}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div>
+              <h3>Our Locations</h3>
+              <p>
+                <strong>Europe:</strong> Kemp House 160, City Road London, EC1V
+                2NX, UK.
+              </p>
+              <p>
+                <strong>America:</strong> 27345 W Sand Lake Rd, STE 210 Office
+                3190 Orlando, FL 32819 US.
+              </p>
+              <div>
+                <strong>Africa:</strong>
+                <p>
+                  Lagos: Dominion Plaza, 140/141 Lekki - Epe Expressway, Lekki,
+                  Lagos 106104, Nigeria.
+                </p>
+                <p>
+                  Ekiti: No.1 JayTee Ojo Crescent, Off Okemesi Road, Ikoro
+                  Ekiti, Ekiti State, Nigeria.
+                </p>
+              </div>
+              <p>
+                <strong>Email:</strong>{" "}
+                <a href="mailto:contact@alluvium.net">contact@alluvium.net</a>{" "}
+              </p>
+              <p>
+                <strong>Phone Number:</strong>{" "}
+                <a href="tel:+442035762028"> +442035762028</a>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </ToastProvider>
+  );
+};
