96205e6be62b462fa203a7d0bf30ac93b837ab87
geekmsd <sherifsiyanbola@gmail.com>
2024-06-19 21:24:58 +0100

    Featured Section
    Made update on the featured section

--- components/Featured.js
+++ components/Featured.js
@@ -15,8 +15,9 @@ export default function () {
         <div>
           <Image className={`img-fluid ${styles.serv}`} width={176} height={101} src="/assets/serv.png" />
         </div>
-        <div className="ml-2">
-          <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:""}}>OUR SERVICES</p>
+        <div className="ml-2 mt-2">
+          <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:"10px", marginLeft:"10px"}}>OUR SERVICES</p>
+          <p className="" style={{fontSize:"10px", fontWeight:"bold", marginTop:"-10px", marginLeft:"10px"}}> See how Alluvium Can Help You</p>
           
         </div>
       </div>
@@ -24,8 +25,9 @@ export default function () {
         <div>
           <Image className={`img-fluid ${styles.serv}`} width={176} height={101} src="/assets/camp.png" />
         </div>
-        <div className="ml-2">
-        <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:""}}>CAMPAIGN</p>
+        <div className="ml-2 mt-2">
+        <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:"10px", marginLeft:"10px"}}>CAMPAIGN</p>
+        <p className="" style={{fontSize:"10px", fontWeight:"bold", marginTop:"-10px", marginLeft:"10px"}}> Enhance Your Jira Management Skills</p>
           
         </div>
       </div>
@@ -33,8 +35,9 @@ export default function () {
         <div>
           <Image className={`img-fluid ${styles.serv}`} width={176} height={101} src="/assets/case.png" />
         </div>
-        <div className="ml-2">
-        <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:""}}>CASE STUDIES</p>
+        <div className="ml-2 mt-2">
+        <p className="" style={{color:"#009BC0", fontSize:"10px", fontWeight:"bold", marginTop:"10px", marginLeft:"10px"}}>CASE STUDIES</p>
+        <p className="" style={{fontSize:"10px", fontWeight:"bold", marginTop:"-10px", marginLeft:"10px"}}> Explore More of Our Transformative Projects</p>
         </div>
       </div>
     </div>
