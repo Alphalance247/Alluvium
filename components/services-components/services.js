cbeac56a63a7d030720cbadda42c0cfb2c5a6541
geekmsd <sherifsiyanbola@gmail.com>
2024-06-25 22:19:22 +0100

    UI Fixes

--- components/services-components/services.js
+++ components/services-components/services.js
@@ -103,7 +103,7 @@ const Services = () => {
 
 
 
-        <div className={`row ${styles.servdiv}`}>
+        <div className={`row ${styles.servaws}`}>
           <div className={`col-md-6 ${styles.servmd}`}>
             <h1 className={`font ${styles.servhead}`}>AWS Consulting</h1>
             <p className={` ${styles.servp}`}>
