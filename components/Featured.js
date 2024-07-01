77fbdec70c574d64ab2535820ef3653dd8e6adfd
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-07-01 17:21:34 +0100

    blog post and route changes

--- components/Featured.js
+++ components/Featured.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/hero-carousel.module.scss";
+
 export default function () {
+  const hadndleScrollToView = (section) => {
+    document.getElementById(section)?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className={`${styles.customxs}`}>
       <div className={`${styles.featured}`}>
@@ -9,7 +14,11 @@ export default function () {
           <h3 className={`${styles.featuredsec} text-xl font-bold`}>
             FEATURED SECTION
           </h3>
-          <div className="col-md-4 d-flex align-items-center">
+          <div
+            className="col-md-4 d-flex align-items-center cursor-pointer"
+            onClick={() => hadndleScrollToView("services")}
+            style={{ cursor: "pointer" }}
+          >
             <div>
               <Image
                 className={`img-fluid ${styles.serv}`}
@@ -45,43 +54,54 @@ export default function () {
               </p>
             </div>
           </div>
-          <div className="col-md-4 d-flex align-items-center">
-            <div>
-              <Image
-                className={`img-fluid ${styles.serv}`}
-                width={176}
-                height={101}
-                src="/assets/camp.png"
-              />
+
+          <Link href="/blog" passHref>
+            <div
+              className="col-md-4 d-flex align-items-center"
+              style={{ cursor: "pointer" }}
+            >
+              <div>
+                <Image
+                  className={`img-fluid ${styles.serv}`}
+                  width={176}
+                  height={101}
+                  src="/assets/camp.png"
+                />
+              </div>
+              <div className="ml-2 mt-2">
+                <p
+                  className=""
+                  style={{
+                    color: "#009BC0",
+                    fontSize: "10px",
+                    fontWeight: "bold",
+                    marginTop: "10px",
+                    marginLeft: "10px",
+                  }}
+                >
+                  CAMPAIGN
+                </p>
+                <p
+                  className=""
+                  style={{
+                    fontSize: "10px",
+                    fontWeight: "bold",
+                    marginTop: "-10px",
+                    marginLeft: "10px",
+                  }}
+                >
+                  {" "}
+                  Enhance Your Jira Management Skills
+                </p>
+              </div>
             </div>
-            <div className="ml-2 mt-2">
-              <p
-                className=""
-                style={{
-                  color: "#009BC0",
-                  fontSize: "10px",
-                  fontWeight: "bold",
-                  marginTop: "10px",
-                  marginLeft: "10px",
-                }}
-              >
-                CAMPAIGN
-              </p>
-              <p
-                className=""
-                style={{
-                  fontSize: "10px",
-                  fontWeight: "bold",
-                  marginTop: "-10px",
-                  marginLeft: "10px",
-                }}
-              >
-                {" "}
-                Enhance Your Jira Management Skills
-              </p>
-            </div>
-          </div>
-          <div className="col-md-4 d-flex align-items-center">
+          </Link>
+
+          <div
+            className="col-md-4 d-flex align-items-center"
+            onClick={() => hadndleScrollToView("case-studies")}
+            style={{ cursor: "pointer" }}
+          >
             <div>
               <Image
                 className={`img-fluid ${styles.serv}`}
---
+++ components/blog-component/lifecycle.jsx
@@ -0,0 +1,24 @@
+const Lifecycle = () => {
+  return (
+    <div>
+      <h3>Your Work Lifecycle</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Integer phasellus mus iaculis
+        in. Consequat venenatis mi nulla sem. Justo sit accumsan diam netus nibh
+        amet amet turpis. Netus quam elementum pellentesque hendrerit nullam.
+        Fames sit quis elementum facilisi proin integer. <br /> <br />
+        Consectetur eu sed in tortor. Id diam egestas lacus pretium mollis est
+        massa. Nunc duis bibendum tristique pharetra ac eget. Arcu adipiscing ac
+        vel lectus dui. Hac pellentesque mauris placerat commodo interdum
+        bibendum. Tincidunt elit sed massa gravida egestas a tincidunt posuere.
+        <br /> <br />
+        Consectetur eu sed in tortor. Id diam egestas lacus pretium mollis est
+        massa. Nunc duis bibendum tristique pharetra ac eget. Arcu adipiscing ac
+        vel lectus dui. Hac pellentesque mauris placerat commodo interdum
+        bibendum. Tincidunt elit sed massa
+      </p>
+    </div>
+  );
+};
+
+export default Lifecycle;
