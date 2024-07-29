c449277991267439801684d04198688f6604f1fd
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-07-29 16:16:51 +0100

    new changes

--- pages/why-hire-us/index.jsx
+++ pages/why-hire-us/index.jsx
@@ -2,7 +2,7 @@ import Layout from "components/layout";
 import ContentTab from "components/blog-component/contentInfo";
 import Potential from "components/blog-component/unlockquote";
 import Articles from "components/blog-component/article";
-import HeroBlog from "components/blog-component/heroblog";
+import HeroBlog from "components/blog-component/heroBlog";
 
 const Whyhire = () => {
   return (
