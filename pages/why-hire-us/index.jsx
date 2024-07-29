e85b47454539c1e88bacddd92d3cdff267af5852
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-07-29 16:07:28 +0100

    resolved conflicts

--- pages/why-hire-us/index.jsx
+++ pages/why-hire-us/index.jsx
@@ -1,5 +1,5 @@
 import Layout from "components/layout";
-import ContentTab from "components/blog-component/contentinfo";
+import ContentTab from "components/blog-component/contentInfo";
 import Potential from "components/blog-component/unlockquote";
 import Articles from "components/blog-component/article";
 import HeroBlog from "components/blog-component/heroblog";
