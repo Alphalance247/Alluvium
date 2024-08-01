71e60033572a9d36a11811d01daad32f805737bc
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-08-01 10:06:10 +0100

    new section added

---
+++ components/cloud-migration-compo/migrationProcess.jsx
@@ -0,0 +1,19 @@
+import styles from "../../styles/licence.module.scss";
+import { PiCircleThin } from "react-icons/pi";
+import { FaRegCircle } from "react-icons/fa";
+{
+  /* <PiCircleThin /> */
+  //   <FaRegCircle />
+}
+
+const MigrationProcess = () => {
+  return (
+    <div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default MigrationProcess;
