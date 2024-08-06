03664630a4eae257e9b80f0fb131388eb4eb322c
israelmitolu <israelmitolu@gmail.com>
2024-08-06 16:13:45 +0100

    feat: Implement Resources submenu

--- components/cloud-connect/Header.jsx
+++ components/cloud-connect/Header.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Image from "next/image";
 import styles from "../../styles/cloudconnect.module.scss";
 import Link from "next/link";
 import Button from "./Button";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const [isResourcesOpen, setIsResourcesOpen] = useState(false);
+
+  const router = useRouter();
+
+  const isActive = (path) => router.pathname === path;
+
   return (
     <header className={styles.header}>
       <nav>
         <div className={styles.logoDiv}>
           <div className={styles.flexBetween}>
             <Image
-              src={"/assets/cloud-connect/logo.png"}
+              src={"/assets/cloud-connect/images/logo.png"}
               alt="Cloud connect logo"
               width={135}
               height={71}
@@ -22,18 +30,52 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
+        <ul className={styles.navList}>
+          <li className={isActive("/cloud-connect") ? styles.active : ""}>
+            <Link href="/cloud-connect">Home</Link>
           </li>
-          <li>
-            <Link href="/agenda">Agenda</Link>
+          <li
+            className={isActive("/cloud-connect/agenda") ? styles.active : ""}
+          >
+            <Link href="/cloud-connect/agenda">Agenda</Link>
           </li>
-          <li>
-            <Link href="/sponsor">Sponsor</Link>
+          <li
+            className={isActive("/cloud-connect/sponsor") ? styles.active : ""}
+          >
+            <Link href="/cloud-connect/sponsor">Sponsor</Link>
           </li>
-          <li>
-            <Link href="/resources">Resources</Link>
+          <li
+            className={`${styles.resources} ${
+              isResourcesOpen ? styles.open : ""
+            }`}
+          >
+            <button onClick={() => setIsResourcesOpen(!isResourcesOpen)}>
+              Resources
+              <Image
+                src={"/assets/cloud-connect/icons/lucide_chevron-down.svg"}
+                alt=""
+                width={16}
+                height={16}
+              />
+            </button>
+            {isResourcesOpen && (
+              <ul className={styles.submenu}>
+                <li
+                  className={
+                    isActive("/cloud-connect/news") ? styles.active : ""
+                  }
+                >
+                  <Link href="/resources/documents">News</Link>
+                </li>
+                <li
+                  className={
+                    isActive("/cloud-connect/media") ? styles.active : ""
+                  }
+                >
+                  <Link href="/cloud-connect/videos">Media</Link>
+                </li>
+              </ul>
+            )}
           </li>
         </ul>
         <Button>Buy Tickets</Button>
