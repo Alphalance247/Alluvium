fff160f993336bf91830f84bb4d51f258e014261
israelmitolu <israelmitolu@gmail.com>
2024-08-08 04:24:18 +0100

    feat: implement media page

--- pages/cloud-connect/media.jsx
+++ pages/cloud-connect/media.jsx
@@ -1,15 +1,93 @@
 import Head from "next/head";
+import Image from "next/image";
 import Layout from "../../components/cloud-connect-2/Layout";
 import SectionHeader from "../../components/cloud-connect-2/SectionHeader";
+import styles from "../../styles/cloud-connect-2/Media/media.module.scss";
 
 export default function Media() {
+  const images = [
+    "/assets/cloud-connect/images/cc23/Image.png",
+    "/assets/cloud-connect/images/cc23/Image-1.png",
+    "/assets/cloud-connect/images/cc23/Image-2.png",
+    "/assets/cloud-connect/images/cc23/Image-3.png",
+    "/assets/cloud-connect/images/cc23/Image-4.png",
+    "/assets/cloud-connect/images/cc23/Image-5.png",
+    "/assets/cloud-connect/images/cc23/Image-6.png",
+    "/assets/cloud-connect/images/cc23/Image-7.png",
+    "/assets/cloud-connect/images/cc23/Image-8.png",
+    "/assets/cloud-connect/images/cc23/Image-9.png",
+    "/assets/cloud-connect/images/cc23/Image-10.png",
+    "/assets/cloud-connect/images/cc23/Image-11.png",
+    "/assets/cloud-connect/images/cc23/Image-12.png",
+    "/assets/cloud-connect/images/cc23/Image-13.png",
+    "/assets/cloud-connect/images/cc23/Image-14.png",
+    "/assets/cloud-connect/images/cc23/Image-15.png",
+    "/assets/cloud-connect/images/cc23/Image-16.png",
+    "/assets/cloud-connect/images/cc23/Image-17.png",
+    "/assets/cloud-connect/images/cc23/Image-18.png",
+    "/assets/cloud-connect/images/cc23/Image-19.png",
+    "/assets/cloud-connect/images/cc23/Image-20.png",
+    "/assets/cloud-connect/images/cc23/Image-21.png",
+    "/assets/cloud-connect/images/cc23/Image-22.png",
+    "/assets/cloud-connect/images/cc23/Image-23.png",
+  ];
+
+  const renderImageRow = (start, end) => {
+    const rowImages = images.slice(start, end);
+    const isMultipleImages = rowImages.length > 1;
+    const rowHeight = isMultipleImages ? 362 : 480;
+    const imageWidth = isMultipleImages
+      ? `calc((100% - ${(rowImages.length - 1) * 4}px) / ${rowImages.length})`
+      : "100%";
+
+    return (
+      <div
+        className={styles.imageRow}
+        style={{ height: `${rowHeight}px`, marginBottom: "4px" }}
+      >
+        {rowImages.map((src, index) => (
+          <div
+            key={index}
+            className={styles.imageWrapper}
+            style={{
+              width: imageWidth,
+              height: "100%",
+              marginRight: index < rowImages.length - 1 ? "4px" : "0",
+            }}
+          >
+            <img
+              src={src}
+              alt={`Cloud Connect image ${start + index + 1}`}
+             className={styles.image}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <Layout>
       <Head>
         <title>Media | Cloud Connect 2024</title>
       </Head>
-      <section className={styles.agendaSection}>
+      <section className={styles.mediaSection}>
         <SectionHeader heading="Media" />
+      </section>
+      <section className={styles.mediaContent}>
+        <div className={styles.container}>
+          <h3 className={styles.mediaHeading}>Cloud connect '23</h3>
+          <div className={styles.imageContainer}>
+            {renderImageRow(0, 3)} {/* row of 3 images */}
+            {renderImageRow(3, 6)} {/* row of 3 images */}
+            {renderImageRow(6, 7)} {/* row of 1 image */}
+            {renderImageRow(7, 11)} {/* row of 4 images */}
+            {renderImageRow(11, 14)} {/* row of 3 images */}
+            {renderImageRow(14, 15)} {/* row of 1 image */}
+            {renderImageRow(15, 20)} {/* row of 5 images */}
+            {renderImageRow(20, 24)} {/* row of 4 images */}
+          </div>
+        </div>
       </section>
     </Layout>
   );
