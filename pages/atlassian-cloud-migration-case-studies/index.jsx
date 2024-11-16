bda6cd41eab6f8baa67f5392137e0993f13ec8a3
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-11-16 13:25:48 +0100

    cloud connect ticket update

--- pages/atlassian-cloud-migration-case-studies/index.jsx
+++ pages/atlassian-cloud-migration-case-studies/index.jsx
@@ -1,7 +1,6 @@
 import CloudMain from "components/cloud-migration-case-study/cloudMain";
 import Layout from "components/layout";
 import CaseStudyCard from "../../components/cloud-migration-case-study/caseStudyCard";
-import HeroCloud from "components/cloud-migration-case-study/hero";
 
 const CloudMigrationCaseStudy = () => {
   return (
