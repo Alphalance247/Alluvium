b6a2dab1801c9e2c5b8c01e37ca0e91462f7d2e9
AbdulAzeez247 <abdulazeez@alluvium.net>
2024-12-11 21:57:28 +0100

    Add Google tracking tag to alluvium website

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,21 @@ export default class MyDocument extends Document {
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16717401169"
+          ></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                 function gtag(){dataLayer.push(arguments);} 
+                 gtag('js', new Date()); 
+                 gtag('config', 'AW-16717401169');
+            `,
+            }}
+          />
+          <script
+            async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
           />
           <script
