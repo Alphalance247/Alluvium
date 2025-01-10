cbfd7a9f72bcd47ac18db3bdf2e4acaea8720ea6
Waterspecial <boluwatifeomirinde080@gmail.com>
2025-01-10 15:58:07 +0100

    startup

--- components/Alluvium-Redesign-2025/startup/SupportPackage.jsx
+++ components/Alluvium-Redesign-2025/startup/SupportPackage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "../../../styles/AlluviumRedesign2025/startup/SupportPackage.module.scss";
+import StartupPackage from "./StartupPackage";
 
 const SupportPackage = () => {
-  return <div>SupportPackage</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <main>
+        <StartupPackage />
+      </main>
+    </div>
+  );
 };
 
 export default SupportPackage;
--- pages/startup/index.jsx
+++ pages/startup/index.jsx
@@ -1,6 +1,5 @@
-import Badges from "components/Alluvium-Redesign-2025/ReuseComponents/badges";
-import Expertise from "components/Alluvium-Redesign-2025/expertise";
 import HeroSection from "components/Alluvium-Redesign-2025/startup/HeroSection";
+import SupportPackage from "components/Alluvium-Redesign-2025/startup/SupportPackage";
 import Layout from "components/layout";
 import React from "react";
 
