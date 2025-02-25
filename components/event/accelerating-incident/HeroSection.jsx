118c2a92700516d9e268e1ab6d97b83090c3270f
Waterspecial <boluwatifeomirinde080@gmail.com>
2025-02-25 11:51:32 +0100

    incident integrations

---
+++ components/event/accelerating-incident/HeroSection.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../../../styles/AlluviumRedesign2025/accelerating-incident/Herosection.module.scss";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section className={styles.mainContainer}>
+      <main>
+        <div className={styles.heroText}>
+          <p className={styles.modal}>ALLUVIUM USER GROUP EVENT</p>
+          <h2>
+            Accelerating Incident <br /> Management Through AI- <br />
+            Assisted Solutions
+          </h2>
+          <p className={styles.discover}>
+            Discover how AI-driven strategies Resolve incidents faster, reduce{" "}
+            <br />
+            downtime, and empower your team with intelligent automation. 
+          </p>
+          <div>
+            <a href="">Register Now</a>
+          </div>
+        </div>
+        <div style={{ marginTop: "64px" }}>
+          <Image
+            src="/assets/redesign-2025/Container.webp"
+            alt="hero image"
+            width={1280}
+            height={361}
+            // layout="fill"
+          />
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default HeroSection;
