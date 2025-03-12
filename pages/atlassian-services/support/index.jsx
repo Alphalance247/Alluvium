7c5483b4233c8247e3293c1a8f07752c7c21b7ee
AbdulAzeez247 <abdulazeez@alluvium.net>
2025-03-12 08:49:35 +0100

    Update support link to direct users to the new support portal

--- pages/atlassian-services/support/index.jsx
+++ pages/atlassian-services/support/index.jsx
@@ -27,7 +27,7 @@ const Support = () => {
         contentsheading2="Be assured of having technical and functional Atlassian expertise by your side with our Atlassian support packages."
         withSlide={false}
         button1={
-          <Link href="/contact-us">
+          <Link href="https://support.portal.alluvium.net/servicedesk/customer/portals">
             <Button size="large">Go to Support</Button>
           </Link>
         }
