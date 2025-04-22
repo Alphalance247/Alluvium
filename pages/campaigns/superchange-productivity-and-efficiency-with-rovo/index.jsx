f991efd3b11da33947988a0e38391d54473bea88
AbdulAzeez247 <abdulazeez@alluvium.net>
2025-04-22 15:21:58 +0100

    Add SuperchangeProductivity and HighVelocity pages; implement LeadForm with respective data URLs Update Carbon Reduction Plan title for consistency

---
+++ pages/campaigns/superchange-productivity-and-efficiency-with-rovo/index.jsx
@@ -0,0 +1,15 @@
+import Layout from "components/layout";
+import LeadForm from "pages/event/itsm-solutions/form/leadform";
+const SuperchangeProductivity = () => {
+  return (
+    <Layout>
+      <LeadForm
+        dataUrl={
+          "https://sites.ziftsolutions.com/atlassian.ziftsolutions.com/8a9993409655128e01965d63bb196bd9"
+        }
+      />
+    </Layout>
+  );
+};
+
+export default SuperchangeProductivity;
