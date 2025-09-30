ed9c94473de22e0fb779b7901c818ff5570e1903
Moshood Azeez <abdulazeez@alluvium.net>
2025-09-30 15:20:45 +0000

    page title description  and title edit

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     <>
       <Layout>
         <Head>
-          <title>Alluvium: |  Atlassian Global Solution Partner </title>
+          <title>Alluvium |  Atlassian Global Solution Partner </title>
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="preload"
@@ -22,11 +22,11 @@ const Home = () => {
           />
           <meta
             name="description"
-            content="Page Description:   Alluvium, an Atlassian Solution Partner in the United States, United Kingdom, and Nigeria. We  deliver ITSM, DevOps, Agile, Cloud Migration, and workflow automation services. "
+            content="Alluvium, an Atlassian Solution Partner in the United States, United Kingdom, and Nigeria. We  deliver ITSM, DevOps, Agile, Cloud Migration, and workflow automation services. "
           />
           <meta
             name="keywords"
-            content=" Atlassian Solution Partner, Atlassian services, ITSM solutions, Jira Service Management,  Confluence, Jira Software, Bitbucket, DevOps tools, Agile workflows, Cloud migration, workflow  automation, project management tools, Atlassian partner US, Atlassian partner UK, Atlassian  partner Nigeria, ITSM Atlassian consultant, enterprise collaboration tools." />
+            content="Atlassian Solution Partner, Atlassian services, ITSM solutions, Jira Service Management,  Confluence, Jira Software, Bitbucket, DevOps tools, Agile workflows, Cloud migration, workflow  automation, project management tools, Atlassian partner US, Atlassian partner UK, Atlassian  partner Nigeria, ITSM Atlassian consultant, enterprise collaboration tools." />
         </Head>
         <Hero />
         <Expertise />
