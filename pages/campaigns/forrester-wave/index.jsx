d7d8819082f534c00a5a67137217ef244b218f00
Abdul Azeez <al.moshood017@gmail.com>
2025-10-28 18:08:54 +0100

    Add ForresterWave page with LeadForm component integration

---
+++ pages/campaigns/forrester-wave/index.jsx
@@ -0,0 +1,15 @@
+import Layout from "components/layout";
+import LeadForm from "pages/event/itsm-solutions/form/leadform";
+const ForresterWave = () => {
+  return (
+    <Layout>
+      <LeadForm
+        dataUrl={
+          "https://sites.ziftsolutions.com/atlassian.ziftsolutions.com/8a9997c59a223fbe019a2466f1fc16ec"
+        }
+      />
+    </Layout>
+  );
+};
+
+export default ForresterWave;
