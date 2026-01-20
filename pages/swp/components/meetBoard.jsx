0a6b86ad5a4421eeeae951587651f5ba109f8f44
Azeez <al.moshood017@gmail.com>
2026-01-20 12:09:04 +0100

    responsiveness implementation

--- pages/swp/components/meetBoard.jsx
+++ pages/swp/components/meetBoard.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import styles from "../../../styles/AlluviumRedesign2025/swp/swp.module.scss";
 
 const MeetBoard = () => {
   return (
     <section className={styles.meet_board}>
-      <div className={styles.container}>
+      <div className={styles.container__meet_board}>
         <div></div>
         <div>
           <h2 className={styles.title}>Meet the Alluvium Team</h2>
@@ -11,9 +12,11 @@ const MeetBoard = () => {
           <div className={styles.team_grid}>
             {/* Team Member 1 */}
             <div className={styles.team_member}>
-              <img
+              <Image
                 src="/assets/SWP/jaytee.png"
                 alt="Team Member 1"
+                width={345}
+                height={315}
                 className={styles.member_image}
               />
               <h3 className={styles.member_name}>Alex Johnson</h3>
@@ -21,8 +24,10 @@ const MeetBoard = () => {
             </div>
             {/* Team Member 2 */}
             <div className={styles.team_member}>
-              <img
+              <Image
                 src="/assets/SWP/wale.png"
+                width={345}
+                height={315}
                 alt="Team Member 2"
                 className={styles.member_image}
               />
