05478d5bb0cc78dd2d35b2d35576273a1e5ff5a8
Abdul Azeez <al.moshood017@gmail.com>
2026-05-04 10:29:46 +0100

    Team 26 catch up banner

--- components/layout.js
+++ components/layout.js
@@ -7,12 +7,14 @@ import ContactSection from "./Alluvium-Redesign-2025/ContactSection";
 import Footer from "./Alluvium-Redesign-2026/footer";
 import CookieConsent from "./Alluvium-Redesign-2025/CookieConsent";
 import Navbar from "./Alluvium-Redesign-2026/common/header";
+import AnnouncementBar from "./Alluvium-Redesign-2026/common/banner/team26CatchUp";
 
 export default function Layout({ children, withoutForm }) {
   return (
     <div>
       {/* <Navigation /> */}
       <Navbar />
+      <AnnouncementBar />
       <div className={styles.layoutBody}>{children}</div>
       {/* {<ConsultationForm />} */}
 
