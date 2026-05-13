dfc149fa9a3a5e5a2b1df273f1529d8dd6a77071
Abdul Azeez <al.moshood017@gmail.com>
2026-05-13 22:48:45 +0100

    update the jira widget

--- pages/jira-widget/index.jsx
+++ pages/jira-widget/index.jsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 export default function JiraWidget() {
   useEffect(() => {
     window.csmWidgetSettings = {
-      widgetId: "1b9fdca4-3c1c-4ad6-8cb2-d8cdd3f5b758",
-      site: "one-atlas-tmwv.atlassian.net",
-      cloudId: "884720a9-0ef3-4650-82ee-aa5a9dfc6965",
+      widgetId: "09e960f0-aa19-4250-9c9e-13727cb70d3a",
+      site: "alluviumdemo.atlassian.net",
+      cloudId: "8b3f9314-316c-4b9a-8470-8b9298e51d38",
     };
 
     if (!document.getElementById("jira-csm-script")) {
@@ -14,7 +14,7 @@ export default function JiraWidget() {
       script.id = "jira-csm-script";
 
       script.src =
-        "https://one-atlas-tmwv.atlassian.net/csm/widget/script.js?widgetId=1b9fdca4-3c1c-4ad6-8cb2-d8cdd3f5b758&site=one-atlas-tmwv.atlassian.net&cloudId=884720a9-0ef3-4650-82ee-aa5a9dfc6965";
+        "https://alluviumdemo.atlassian.net/csm/widget/script.js?widgetId=09e960f0-aa19-4250-9c9e-13727cb70d3a&site=alluviumdemo.atlassian.net&cloudId=8b3f9314-316c-4b9a-8470-8b9298e51d38";
 
       script.async = true;
 
@@ -26,7 +26,25 @@ export default function JiraWidget() {
     <div
       style={{
         minHeight: "100vh",
+        position: "relative",
       }}
-    />
+    >
+      <div
+        style={{
+          position: "fixed",
+          bottom: "40px",
+          right: "90px",
+          background: "#fff",
+          padding: "10px 14px",
+          borderRadius: "12px",
+          // boxShadow: "0 4px 12px rgba(0,0,0,0.15)",
+          fontFamily: "Arial",
+          fontSize: "14px",
+          zIndex: 9999,
+        }}
+      >
+        Chat to Tally 👉🏾
+      </div>
+    </div>
   );
 }
