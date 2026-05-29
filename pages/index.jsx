c3ed6caa2f61db653532b8375750ba2744d9f3ca
Abdul Azeez <al.moshood017@gmail.com>
2026-05-29 15:44:50 +0100

    new message

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,7 @@ import SuccessStories from "components/Alluvium-Redesign-2026/home/caseStudyCard
 import CtaSection from "components/Alluvium-Redesign-2026/common/ctaSection";
 import AiAdoptionHero from "components/Alluvium-Redesign-2026/common/aiAdoptionHero";
 import SuccessStory from "components/Alluvium-Redesign-2026/successStory";
+import GifHero from "components/Alluvium-Redesign-2026/home-hero/gifHero";
 
 const badgesImages = [
   {
@@ -58,7 +59,8 @@ const Home = () => {
         {/* <Hero /> */}
         {/* <SWPHero /> */}
         {/* <Team26Hero /> */}
-        <AiAdoptionHero />
+        {/* <AiAdoptionHero /> */}
+        <GifHero />
 
         {/* <ServiceHero
           badges={badgesImages}
