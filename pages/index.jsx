26c79928b05ebc1379f849911a3140a47139e067
Abdul Azeez <al.moshood017@gmail.com>
2026-06-16 09:44:30 +0100

    update GifHero component with new GIF and adjust layout styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,6 @@ import SuccessStories from "components/Alluvium-Redesign-2026/home/caseStudyCard
 import CtaSection from "components/Alluvium-Redesign-2026/common/ctaSection";
 import AiAdoptionHero from "components/Alluvium-Redesign-2026/common/aiAdoptionHero";
 import SuccessStory from "components/Alluvium-Redesign-2026/successStory";
-import GifHero from "components/Alluvium-Redesign-2026/home-hero/gifHero";
 import FeaturedProgramme from "components/Alluvium-Redesign-2026/featuredProgramme/featuredBanner";
 import GifHero from "components/Alluvium-Redesign-2026/home-hero/GifHero";
 import Hero from "components/Alluvium-Redesign-2026/home/Hero";
