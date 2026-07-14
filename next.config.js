179ec15af81f30fd2fb6396cd41132016765c69c
Abdul Azeez <al.moshood017@gmail.com>
2026-07-14 14:13:58 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Removed @zeit/next-sass from dependencies
    - Added autoprefixer, postcss, and tailwindcss to devDependencies
    - Created postcss.config.js to configure Tailwind CSS and autoprefixer

--- next.config.js
+++ next.config.js
@@ -1,83 +1,30 @@
 const path = require("path");
-const withSass = require("@zeit/next-sass");
-const withImages = require("next-images");
-
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios 
-   here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-
-  cssModules: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-});
-
-// module.exports = {
-//   /* Add Your Scss File Folder Path Here */
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'styles')],
-//   },
-// }
 
 module.exports = {
-  basePath: "/docs",
-};
-
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       issuer: {
-//         test: /\.(js|ts)x?$/,
-//       },
-//       use: ['@svgr/webpack'],
-//     });
-
-//     return config;
-//   },
-// };
-
-// module.exports = withImages({
-//   exclude: path.resolve(__dirname, 'public/assets'),
-//   webpack(config, options) {
-
-//     // config.module.rules.push({
-//     //   test: /\.svg$/,
-//     //   issuer: {
-//     //     test: /\.(js|ts)x?$/,
-//     //   },
-//     //   use: ["@svgr/webpack"],
-//     // });
-
-//     return config
-//   }
-// })
-
-module.exports = {
+  // basePath: "/docs",
   swcMinify: true,
+
   env: {
     NEXT_PUBLIC_SMTP_USERNAME: process.env.NEXT_PUBLIC_SMTP_USERNAME,
     NEXT_PUBLIC_SMTP_PASS: process.env.NEXT_PUBLIC_SMTP_PASS,
     AMPLIFY_NEXTJS_EXPERIMENTAL_TRACE:
       process.env.AMPLIFY_NEXTJS_EXPERIMENTAL_TRACE,
   },
-};
 
-module.exports = {
   images: {
-    // loader:"cloudinary",
-    // path:"https://res.cloudinary.com/geniesys",
     domains: ["blog.alluvium.net", "pest.ec2.alluvium.net"],
   },
-};
 
-module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+
   async redirects() {
     return [
       {
         source: "/support/contact",
         destination: "/contact-us",
-        permanent: true, // triggers 308
+        permanent: true,
       },
     ];
   },
