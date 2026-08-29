d5cd139e308286452dc706a767b592564523e142
Abdul Azeez <al.moshood017@gmail.com>
2026-08-29 21:03:01 +0100

    feat: Comment out unused scripts in RootLayout for cleaner code

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,36 +37,36 @@ export default function RootLayout({ children }) {
           async
           defer
         ></script>
-        // <script
-        //   defer
-        //   async
-        //   src="//ukbrowserspeed.com/_k/t.js"
-        //   data-public-key="pk_fd206eefacbe3bc36ef10b8742ef49b0"
-        // ></script>
-        // <script
-        //   dangerouslySetInnerHTML={{
-        //     __html: `
-        //       window.csmWidgetSettings = {
-        //         widgetId: "900edb70-b64d-4204-8c22-707b4c7d5336",
-        //         site: "one-atlas-jdkb.atlassian.net",
-        //         cloudId: "ceaca2eb-b3c0-4d39-9b74-531ec473bf10",
-        //       }
-        //     `,
-        //   }}
-        // />
-        // <script
-        //   dangerouslySetInnerHTML={{
-        //     __html: `
-        //       (function(){
-        //         if (typeof window.CSM === "undefined") {
-        //           var CSM = function(){CSM.__q__.push(arguments);};CSM.__q__=[];window.CSM=CSM;
-        //           var l = function() {var s = document.createElement('script');s.type="text/javascript";s.async=true;s.src="https://one-atlas-jdkb.atlassian.net/csm/widget/script.js?widgetId=900edb70-b64d-4204-8c22-707b4c7d5336&site=one-atlas-jdkb.atlassian.net&cloudId=ceaca2eb-b3c0-4d39-9b74-531ec473bf10";document.body.appendChild(s);}
-        //           if(document.readyState==="complete"){l();}else{window.addEventListener('load',l);}
-        //         }
-        //       })()
-        //     `,
-        //   }}
-        // />
+        {/* <script
+          defer
+          async
+          src="//ukbrowserspeed.com/_k/t.js"
+          data-public-key="pk_fd206eefacbe3bc36ef10b8742ef49b0"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.csmWidgetSettings = {
+                widgetId: "900edb70-b64d-4204-8c22-707b4c7d5336",
+                site: "one-atlas-jdkb.atlassian.net",
+                cloudId: "ceaca2eb-b3c0-4d39-9b74-531ec473bf10",
+              }
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){
+                if (typeof window.CSM === "undefined") {
+                  var CSM = function(){CSM.__q__.push(arguments);};CSM.__q__=[];window.CSM=CSM;
+                  var l = function() {var s = document.createElement('script');s.type="text/javascript";s.async=true;s.src="https://one-atlas-jdkb.atlassian.net/csm/widget/script.js?widgetId=900edb70-b64d-4204-8c22-707b4c7d5336&site=one-atlas-jdkb.atlassian.net&cloudId=ceaca2eb-b3c0-4d39-9b74-531ec473bf10";document.body.appendChild(s);}
+                  if(document.readyState==="complete"){l();}else{window.addEventListener('load',l);}
+                }
+              })()
+            `,
+          }}
+        /> */}
       </head>
       <body>
         <ScrollToTop />
